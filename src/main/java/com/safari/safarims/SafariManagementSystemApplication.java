b6b036394372938358a04e55e5c0ea02be8e4e66
Senuja Thisum <senujathisumekanayake@gmail.com>
2025-10-01 16:46:33 +0530

    initial project

---
+++ src/main/java/com/safari/safarims/SafariManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.safari.safarims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafariManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafariManagementSystemApplication.class, args);
+    }
+
+}
